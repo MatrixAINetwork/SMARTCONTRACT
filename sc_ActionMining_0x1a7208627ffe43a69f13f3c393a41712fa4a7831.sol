c5a0d9b5a538ade32be3e33602916d2d989a6d51
matrixhaolin <linhao@matrix.space>
2018-08-24 17:59:50 +0800

    prepared sol files for MATRIX AI TEAM

---
+++ sc_ActionMining_0x1a7208627ffe43a69f13f3c393a41712fa4a7831.sol
@@ -0,0 +1,7 @@
+#These solidity codes have been obtained from Etherscan for extracting the smartcontract related info. The data will be used by MATRIX AI team as the reference basis for MATRIX model analysis, extraction of contract semantics, as well as AI based data analysis, etc.
+/* ==================================================================== */
+/* Copyright (c) 2018 The ether.online Project.  All rights reserved.
+/* 
+/* https://ether.online  The first RPG game of blockchain 
+/*  
+/* authors 
