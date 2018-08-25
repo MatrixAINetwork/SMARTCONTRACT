ead22f3ea1b5c32f9e7400d97eaca675d2067e42
matrixhaolin <linhao@matrix.space>
2018-08-25 15:30:17 +0800

    sol reference files uploaded

---
+++ Reference SOLIDITY/sc_BRT_GIFT_0x26ae986bFab33F4CBaDeC30Ea55b5eeD9E883ecf.sol
@@ -0,0 +1,61 @@
+/**********************************************************************
+*These solidity codes have been obtained from Etherscan for extracting
+*the smartcontract related info.
+*The data will be used by MATRIX AI team as the reference basis for
+*MATRIX model analysis,extraction of contract semantics,
+*as well as AI based data analysis, etc.
+**********************************************************************/
+pragma solidity ^0.4.20;
+
+contract BRT_GIFT
+{
+    address sender;
+    
+    address reciver;
+    
+    bool closed = false;
+    
+    uint unlockTime;
+ 
+    function PutGift(address _reciver)
+    public
+    payable
+    {
+        if( (!closed&&(msg.value > 1 ether)) || sender==0x00 )
+        {
+            sender = msg.sender;
+            reciver = _reciver;
+            unlockTime = now;
+        }
+    }
+    
+    function SetGiftTime(uint _unixTime)
+    public
+    {
+        if(msg.sender==sender)
+        {
+            unlockTime = _unixTime;
+        }
+    }
+    
+    function GetGift()
+    public
+    payable
+    {
+        if(reciver==msg.sender&&now>unlockTime)
+        {
+            msg.sender.transfer(this.balance);
+        }
+    }
+    
+    function CloseGift()
+    public
+    {
+        if(sender == msg.sender && reciver != 0x0 )
+        {
+           closed=true;
+        }
+    }
+    
+    function() public payable{}
+}
